ea8a715c1b3f18804ce1032a7edd4a836adda47e
Flynn <wphestiraid@gmail.com>
2019-10-17 20:58:37 +0900

    기본 User 모델 생성

--- app/models/User.py
+++ app/models/User.py
@@ -1,13 +1,21 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Text
+from werkzeug.security import generate_password_hash, check_password_hash
 from database import Base
 
 
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    email = Column(String(120), unique=True)
+    email = Column(String(120), index=True, unique=True)
     name = Column(String(50))
+    password = Column(String(128))
+    bio = Column(Text())
 
-    def __init__(self, name=None, email=None):
+    def __init__(self, email, name, password, bio=None):
         self.email = email
         self.name = name
+        self.password = generate_password_hash(password)
+        self.bio = bio
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
